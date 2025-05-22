2ee49d972d6fe59e78771c7a8e520be54de1c1c5
michal <m05567271587@gmail.com>
2025-05-22 17:59:56 +0300

    update logo

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -5,7 +5,7 @@ import { AppBar, Toolbar, Button } from "@mui/material"
 import HomeIcon from "@mui/icons-material/Home"
 import HistoryIcon from "@mui/icons-material/History"
 import EmailIcon from "@mui/icons-material/Email"
-import Logo from "../../features/home/components/logo"
+import Logo from "../pages/logo"
 const Navbar = () => {
   return (
     <AppBar
