642a67cc18ee7da8ba1a62a7d1a32db3ce643da9
michal <m05567271587@gmail.com>
2025-06-08 01:57:40 +0300

    change update

--- PhotoChallenge/PhotoChallenge.Data/Repositories/UserRepository.cs
+++ PhotoChallenge/PhotoChallenge.Data/Repositories/UserRepository.cs
@@ -53,6 +53,7 @@ namespace PhotoChallenge.Data.Repositories
 
             existingUser.Name = user.Name;
             existingUser.Email = user.Email;
+            existingUser.Password = user.Password;
             existingUser.Role = user.Role;
             existingUser.IsDeleted = user.IsDeleted;
             _dataContext.Users.Update(existingUser);
