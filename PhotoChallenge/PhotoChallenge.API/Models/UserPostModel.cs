ddd0521c51db5ec4abb8f07f7378b0f970e5eb6d
michal <m05567271587@gmail.com>
2025-06-08 15:29:03 +0300

    change

--- PhotoChallenge/PhotoChallenge.API/Models/UserPostModel.cs
+++ PhotoChallenge/PhotoChallenge.API/Models/UserPostModel.cs
@@ -1,9 +1,12 @@
-﻿namespace PhotoChallenge.API.Models
+﻿using PhotoChallenge.Core.Models;
+
+namespace PhotoChallenge.API.Models
 {
     public class UserPostModel
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string? Password { get; set; }
+        public ERole? Role { get; set; }
     }
 }
