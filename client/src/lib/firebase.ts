9884f3ff72064979066fe442bd8b40fa68f952bc
m1587 <05567271587m@gmail.com>
2026-06-25 21:14:50 +0300

    Update Firebase configuration details

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -2,15 +2,15 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-  const firebaseConfig = {
-  apiKey: "AIzaSyCVE2xdvFCg8OWoNYDT5F82M38_KPUpVzY",
-  authDomain: "photochallenge-f053c.firebaseapp.com",
-  projectId: "photochallenge-f053c",
-  storageBucket: "photochallenge-f053c.firebasestorage.app",
-  messagingSenderId: "297899099437",
-  appId: "1:297899099437:web:7b709b57a679a02446bfa4"
-};
-
+const firebaseConfig = {
+  apiKey: "AIzaSyC4sNDaLtqJQ8LVvAheIt3szcWVUI_B7_A",
+  authDomain: "potochallenge-fb28f.firebaseapp.com",
+  projectId: "potochallenge-fb28f",
+  storageBucket: "potochallenge-fb28f.firebasestorage.app",
+  messagingSenderId: "507651210080",
+  appId: "1:507651210080:web:de8e154d0956a1a45cbc05"
+}
 
 const app = initializeApp(firebaseConfig)
+
 export const db = getFirestore(app)
